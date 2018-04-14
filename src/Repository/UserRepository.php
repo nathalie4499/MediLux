91f9b5ae66529c3a996b9e8ce7fc7017de300b92
D4rkPh0en1x <frank@datateam.lu>
2018-04-14 13:59:59 +0200

    Login activation with argon2i & created base search controller for Nadia for my testing purposes

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -12,13 +12,27 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
+   
 class UserRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, User::class);
     }
-
+    
+    public function usernameExist(string $username)
+    {
+        $queryBuilder = $this->createQueryBuilder('u');
+        
+        $queryBuilder->select('COUNT(u) AS count')
+        ->where('u.username = :username')
+        ->setParameter('username', $username);
+        
+        $result = $queryBuilder->getQuery()->getOneOrNullResult();
+        
+        return boolval($result['count']);
+    }
+    
 //    /**
 //     * @return User[] Returns an array of User objects
 //     */
