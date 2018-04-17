e5fe4906e0a92ad98d2bf93665dbf52527695fc2
sznorbee <norbert.szekeres@gmail.com>
2018-04-17 15:43:14 +0200

    add doctors sans address, sans valid ok

--- src/Controller/AddressbookController.php
+++ src/Controller/AddressbookController.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Types\IntegerType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -19,6 +20,7 @@ use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Doctors;
 
 
 class AddressbookController extends Controller
@@ -41,5 +43,102 @@ class AddressbookController extends Controller
                     'controller_name' => 'PatientController',
                 ])
             );
+    }
+    
+    public function addDoctor(Environment $twig,
+        FormFactoryInterface $factory,
+        Request $request,
+        ObjectManager $manager,
+        SessionInterface $session,
+        UrlGeneratorInterface $urlGenerator
+        )
+    {
+        $doctor = new Doctors();
+        $builder = $factory->createBuilder(FormType::class, $doctor);
+        $builder->add(
+                      'firstname', TextType::class,
+                         ['label' => 'FORM.ADDRESSBOOK.FIRSTNAME']
+                     )
+                ->add(
+                      'lastname', TextType::class,
+                         ['required' => false,
+                          'label' => 'FORM.ADDRESSBOOK.LASTNAME',
+                         ]
+                     )
+                ->add(
+                      'specialization', TextType::class,
+                      ['label' => 'FORM.ADDRESSBOOK.SPECIALIZATION']
+                     )
+                ->add(
+                      'telwork', TextType::class,
+                      ['label' => 'FORM.ADDRESSBOOK.TELWORK']
+                      )
+                ->add(
+                      'telpriv', TextType::class,
+                      ['required' => false,
+                          'label' => 'FORM.ADDRESSBOOK.TELPRIV',
+                      ]
+                      )
+                ->add(
+                      'mobile', TextType::class,
+                      ['label' => 'FORM.ADDRESSBOOK.MOBILE']
+                      )
+                ->add(
+                      'email', TextType::class,
+                      ['label' => 'FORM.ADDRESSBOOK.EMAIL']
+                      )
+                ->add(
+                      'fax', TextType::class,
+                      ['required' => false,
+                         'label' => 'FORM.ADDRESSBOOK.FAX',
+                      ]
+                      )
+                ->add(
+                      'language', TextType::class,
+                      ['label' => 'FORM.ADDRESSBOOK.LANGUAGE']
+                      )
+                ->add(
+                      'title', TextType::class,
+                      ['label' => 'FORM.ADDRESSBOOK.TITLE']
+                      )
+//                 ->add(
+//                       'address', TextType::class,
+//                       ['required' => false,
+//                          'label' => 'FORM.ADDRESSBOOK.ADDRESS',
+//                       ]
+//                       )
+                         
+                ->add(
+                      'submit', SubmitType::class,
+                        ['attr' => ['class' => 'btn btn-success btn-block'],
+                            'label' => 'FORM.ADDRESSBOOK.SUBMIT'
+                        ]
+                      );
+                    
+                    $form = $builder->getForm();
+                    
+                    
+                    $form->handleRequest($request);
+                    if ($form->isSubmitted() && $form->isValid())
+                    {
+                        $manager->persist($doctor);
+                        $manager->flush();
+                        
+                        $session->getFlashBag()->add('info', 'Ok, New contact is registered!');
+                        
+                        return new RedirectResponse
+                        (
+                            $urlGenerator->generate('addressbook_list')
+                            );
+                    }
+                    
+                    return new Response
+                    (
+                        $twig->render(
+                            'Modules/Addressbook/addressbookAdd.html.twig',
+                            ['formular' => $form->createView()]
+                            )
+                        );
+                    
     }
 }
