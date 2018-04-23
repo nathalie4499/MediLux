f6b36e4308d1489040e2ea41ea4776838cc78e62
Larry <lawrencedampierre@gmail.com>
2018-04-23 16:33:45 +0200

    Create works, added the basis for delete and added a modal

--- src/Controller/DrugsController.php
+++ src/Controller/DrugsController.php
@@ -20,6 +20,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\DrugsRepository;
+use App\Entity\Drugs;
 
 
 class DrugsController extends Controller
@@ -33,13 +34,101 @@ class DrugsController extends Controller
                 ])
             );
     }
-    public function drugsAdd(Environment $twig)
+    public function drugsAdd(Environment $twig,
+        FormFactoryInterface $factory,
+        Request $request,
+        SessionInterface $session,
+        UrlGeneratorInterface $urlGenerator,
+        ObjectManager $manager)
     {
+        $drug = new Drugs();
+        
+        $builder = $factory->createBuilder(FormType::class, $drug);
+        $builder->add(
+            'name', TextType::class,
+            ['label' => 'FORM.DRUGS.NAME']
+            )
+            ->add(
+                'supplier', TextType::class,
+                ['required' => false,
+                    'label' => 'FORM.DRUGS.SUPPLIER',
+                ]
+                )
+                ->add(
+                    'dosage', TextType::class,
+                    ['label' => 'FORM.DRUGS.DOSAGE']
+                    )
+                    ->add(
+                        'contraindications', TextType::class,
+                        ['label' => 'FORM.DRUGS.CONTRAINDICATIONS']
+                        )
+                        ->add(
+                            'sideeffects', TextType::class,
+                            ['required' => false,
+                                'label' => 'FORM.DRUGS.SIDEEFFECTS',
+                            ]
+                            )
+                            ->add(
+                                'incompatibilities', TextType::class,
+                                ['label' => 'FORM.DRUGS.INCOMPATIBILITIES']
+                                )
+                                ->add(
+                                    'overdose', TextType::class,
+                                    ['label' => 'FORM.DRUGS.OVERDOSE']
+                                    )
+                                    ->add(
+                                        'components', TextType::class,
+                                        ['required' => false,
+                                            'label' => 'FORM.DRUGS.COMPONENTS',
+                                        ]
+                                        )
+            //end address
+        ->add(
+            'submit', SubmitType::class,
+            ['attr' => [
+                'class' => 'btn btn-success btn-block',
+                'stateless' => false
+            ],
+                'label' => 'FORM.ADDRESSBOOK.SUBMIT'
+            ]
+            );
+        
+        $form = $builder->getForm();
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            var_dump($drug);
+            $manager->persist($drug);
+            var_dump($drug);
+            $manager->flush();
+            
+            $session->getFlashBag()->add('info', 'Ok, New contact is registered!');
+            return new RedirectResponse
+            (
+                $urlGenerator->generate('addressbook_list')
+                );
+        }
         return new Response(
             $twig->render(
                 'Modules/Drugs/drugsAdd.html.twig', [
-                    'controller_name' => 'PatientController',
+                    'drugsFormular' => $form->createView(),
                 ])
             );
     }
+    /**
+    public function deleteAction(Request $request, User $userid)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
+        if ($userid === null) {
+            return $this->redirectToRoute('admin_user');
+        }
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($userid);
+        $em->flush();
+        return $this->redirectToRoute('admin_user');
+    }
+    **/
 }
